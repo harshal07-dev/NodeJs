61fe126e02ece0a8f398e942db927525811fb4f8
harshal07-dev <harshalsys845@gmail.com>
2025-06-10 15:13:51 +0530

    learned parsing request,streams etc

--- lecture_02-first node program/first.js
+++ lecture_02-first node program/first.js
@@ -1,8 +1,8 @@
-// const fs = require("fs");
-// fs.writeFile("output.txt", "Writing the first sentence.", (err) => {
-//   if (err) console.log("Error occured");
-//   else console.log("Files written succesfully");
-// });
+const fs = require("fs");
+fs.writeFile("output.txt", "Writing the first sentence.", (err) => {
+  if (err) console.log("Error occured");
+  else console.log("Files written succesfully");
+});
 
 const fs = require("fs");
 fs.writeFile("text.txt", "Testing", (err) => {
